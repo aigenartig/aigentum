8dfe7f6fc0333924b739ee15169dea76a4735a0a
aigenartig <mail2minder@aol.de>
2025-08-18 16:31:54 +0200

    cors ports

--- user-service/src/main/java/com/cloudwebshop/userservice/config/WebConfig.java
+++ user-service/src/main/java/com/cloudwebshop/userservice/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173","http://localhost:8081","http://localhost:8082")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
