c8a5c9e17866991c86ef25c41493687a1c9db5ed
aigenartig <mail2minder@aol.de>
2025-08-26 11:45:08 +0200

    Bugfix - misssing import

--- order-service/src/test/java/com/cloudwebshop/orderservice/service/OrderServiceImplTest.java
+++ order-service/src/test/java/com/cloudwebshop/orderservice/service/OrderServiceImplTest.java
@@ -14,6 +14,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Optional;
